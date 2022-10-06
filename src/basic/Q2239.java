acc875fdac6c172c59d029f97d2695f60c787738
Byeongsu KIM <brightdev.bs@gmail.com>
2022-10-06 23:15:11 +0900

    다이나믹 프로그래밍

---
+++ src/basic/Q2239.java
@@ -0,0 +1,33 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Q2239 {
+
+    static int[] dy;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        dy = new int[k + 1];
+
+        int[] coins = new int[n];
+        for(int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        dy[0] = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = coins[i]; j <= k; j++) {
+                if(dy[j - coins[i]] > 0) {
+                    dy[j] += dy[j - coins[i]];
+                }
+            }
+        }
+
+        System.out.println(dy[k]);
+    }
+}
