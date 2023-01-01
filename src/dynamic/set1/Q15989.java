fffe0c84928afc5baa3ca48ef902160fd17fcffc
Byeongsu KIM <brightdev.bs@gmail.com>
2023-01-01 10:31:07 +0900

    Dp

---
+++ src/dynamic/set1/Q15989.java
@@ -0,0 +1,36 @@
+package dynamic.set1;
+
+import java.util.Scanner;
+
+public class Q15989 {
+
+    public static int[][] dp = new int[100001][4];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for(int i = 4; i <= 10000; i++) {
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+        }
+
+        for (int i : arr) {
+            System.out.println(dp[i][1] + dp[i][2] + dp[i][3]);
+        }
+
+    }
+}
